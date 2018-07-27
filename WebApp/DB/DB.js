20bdc6f865fb2b86bafcc57148c9ae88c42df78f
Lee Bilu <bilu30@gmail.com>
2018-07-27 17:09:04 +0300

    changes

--- WebApp/DB/DB.js
+++ WebApp/DB/DB.js
@@ -1,8 +1,3 @@
-// Creating new Array
-let array = [];
-let userData = {};
-let mappingRandToCookieNumber = {};
-
 let express = require('express');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
@@ -15,141 +10,154 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-
 let server = app.listen(3000, function () {
-    let host = server.address().address
-    let port = server.address().port
+    let host = server.address().address;
+    let port = server.address().port;
     loadDBData();
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log(users_id);
+    console.log("DB listening at http://%s:%s", host, port)
 });
-//Login
+/**
+ * Login
+ * params: email , password
+ */
 app.post('/users/login', function (req, res) {
     let body = req.body;
-    let users = getFromFile("users.txt");
+    let users = getFromFile("users");
     if(!users){
-        res.send(0);
+        res.json({"type" : 0});
         return;
     }
     for(let id in users) {
         if(users[id].email == body.email && users[id].password==body.password){
-            res.json(users[id]);
+            res.json({"type": 1 , "data" :users[id]});
             return;
         }
     }
 
-    res.send(0);
+    res.json({"type" : 0});
     return;
 
 });
-//Register
+/**
+ * Register
+ * params: email , password, username, phone
+ */
 app.post('/users/register', function (req, res) {
         let body = req.body;
-        let users = getFromFile("users.txt");
+        let users = getFromFile("users");
         if (!users) {
-            res.send(0);
+            res.json({"type" : 0});
             return;
         }
         for(let id in users){
             if(users[id].email == body.email){
-                res.send(0);
+                res.json({"type" : 0});
                 return;
             }
         }
 
         users[users_id] = {"id":users_id, "email":body.email, password:body.password, "fullname": body.username, "phone": body.phone, "groups_id" : []};
-        if(!insertToFile(users, "users.txt", users_id)){
-            res.send(0);
+        if(!insertToFile(users, "users", users_id)){
+            res.json({"type" : 0});
             return;
         }
         updateDBData();
-        res.send(1);
+        res.json({"type" : 1});
         return;
         
     });
 
-//Get user
+/**
+ * Get user data
+ * params: id
+ */
 app.post('/users/user', function (req, res) {
     let body = req.body;
-    let users = getFromFile("users.txt");
+    let users = getFromFile("users");
     if(!users){
-        res.send(0);
+        res.json({"type" : 0});
         return;
     }
-    for(let id in users) {
-        if(id == body.id){
-            res.json(users[id]);
-            return;
-        }
+    if(!users[body.id]){
+        res.json({"type" : 0});
+        return;
     }
-
-    res.send(0);
-    return;
+    res.json({"type" : 1, "data" : users[body.id]});
 });
+/**
+ * New group
+ * params: name, user_id
+ */
 
-//New group
 app.post('/groups/add', function (req, res) {
     let body = req.body;
-    let groups = getFromFile("groups.txt");
-    let users = getFromFile("users.txt");
+    let groups = getFromFile("groups");
+    let users = getFromFile("users");
     if(!groups){
-        res.end(0);
+        res.json({"type" : 0});
         return;
     }
     if(!users){
-        res.end(0);
+        res.json({"type" : 0});
         return;
     }
     groups[group_id] = {"id" : group_id, "name" : body.name, "admin_id" : body.user_id};
-    if(!insertToFile(groups, "groups.txt", group_id)){
-        res.send(0);
+    if(!insertToFile(groups, "groups", group_id)){
+        res.json({"type" : 0});
         return;
     }
-    for(let id in users){
-        if(id == body.user_id){
-            users[id].groups.push(group_id-1);
-        }
+    if(!users[body.user_id]){
+        res.json({"type" : 0});
+        return;
     }
-    updateDBData();
-    res.send(1);
-});
 
-//Update group users
+    users[body.user_id].groups.push(group_id-1);
+    updateDBData();
+    res.json({"type" : 1});
+});
+/**
+ * Update group users
+ * params: group_id, emails (array of emails)
+ */
 app.post('/groups/update', function (req, res) {
     let body = req.body;
-    let users = getFromFile("users.txt");
+    let users = getFromFile("users");
     if(!users){
-        res.end(0);
+        res.json({"type" : 0});
     }
     for(let id in users){
         if(body.emails.includes(users[id].email)){
             users[id].groups.push(body.group_id);
         }
     }
-    if(!insertToFile(users,"users.txt", false)){
-        res.send(0);
+    if(!insertToFile(users,"users", false)){
+        res.json({"type" : 0});
         return;
     }
-    res.send(1);
+    res.json({"type" : 1});
     return;
 });
+/**
+ * Get group data by group id
+ * params: group_id
+ */
 
-//Get group data by group id
 app.post('/groups/get', function (req, res) {
     let body = req.body;
-    let groups = getFromFile("groups.txt");
-    let users = getFromFile("users.txt")
+    let groups = getFromFile("groups");
+    let users = getFromFile("users");
     if(!groups || ! users){
-        res.end(0);
+        res.json({"type" : 0});
         return;
     }
     let group_data = {};
-    let admin_id = 0;
-    for(let id in groups){
-        if(groups[id] == body.group_id){
-            group_data['name'] = groups[id].name;
-            admin_id =groups[id].admin_id;
-        }
+    if(!groups[body.group_id]){
+        res.json({"type" : 0});
+        return;
     }
+    group_data['name'] = groups[body.group_id].name;
+    let admin_id =groups[body.group_id].admin_id;
     let users_to_send = [];
     for(let id in users){
         let user = {};
@@ -166,30 +174,69 @@ app.post('/groups/get', function (req, res) {
         users_to_send.push(user);
     }
 
-    res.json(users_to_send);
+    res.json({"type" : 1, "data": users_to_send});
+});
+/**
+ * Get all groups of a user
+ * params: user_id
+ */
+app.post('/groups/getall', function (req, res) {
+    let body = req.body;
+    let users = getFromFile("users");
+    let groups = getFromFile("groups");
+    if(!users || ! groups){
+        res.json({"type" : 0});
+    }
+
+    if(!users[body.user_id]){
+        res.json({"type" : 0});
+    }
+    let groups_to_return = [];
+    for(let id in groups){
+        if(users[body.user_id].groups_id.includes(id)){
+            groups_to_return.push(groups[id]);
+        }
+    }
+
+    res.json({"type": 1, "data" : groups_to_return});
+});
+
+/**
+ * Get all products
+ * params:
+ */
+app.post('/products/get', function (req, res) {
+    let products = getFromFile("products");
+    if(!products){
+        res.json({"type" : 0});
+        return
+    }
+    res.json({"type" : 1, "data" : products});
+});
+
+
+app.post('/orders/newcart', function (req, res) {
+
 });
 
 
 
-
 let loadDBData = function () {
-    fs.readFile("DBdata.text", function (err, data) {
-        if(err){
-            console.log("DBdata error");
-        }
-        try{
-            let db_data = JSON.parse(data);
-            users_id = db_data.users_id;
-            group_id = db_data.group_id;
-        } catch {
-            console.log("DBdata error");
-        }
-    })
+    try{
+        let data = fs.readFileSync("DBdata").toString();
+        let db_data = JSON.parse(data);
+        users_id = db_data.users_id;
+        group_id = db_data.group_id;
+    }
+    catch{
+        return false;
+    }
+
 };
 
 let updateDBData = function(){
     let data = {"users_id" : users_id, "group_id" : group_id};
-    fs.writeFile("DBdata.text", data, 'utf8', function (err) {
+    fs.writeFileSync("DBdata", data, 'utf8', function (err) {
         if(err){
             return false;
         }
@@ -199,7 +246,7 @@ let updateDBData = function(){
 
 
 let insertToFile = function(data, filename, counter) {
-    fs.writeFile(filename, data, 'utf8', function (err) {
+    fs.writeFileSync(filename, data, 'utf8', function (err) {
         if(err){
             return false;
         }
@@ -212,16 +259,10 @@ let insertToFile = function(data, filename, counter) {
 
 
 let getFromFile = function (filename) {
-    fs.readFile(filename, function (err, data) {
-        if(err){
-            return false;
-        }
-        try{
-            let db_data = JSON.parse(data);
-            return db_data;
-
-        } catch{
-            return false;
-        }
-    })
+    try{
+        let data = fs.readFileSync(filename).toString();
+        return JSON.parse(data);
+    } catch{
+        return false;
+    }
 };
