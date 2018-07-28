a43e86c7f4780be5ebc15c9558cc8cf942a06aff
Ofir Lapid <ofiriro3@gmail.com>
2018-07-28 16:08:46 +0300

    Nav-Bar was added to profilePage. PaymentMethod - an options to pay in cash was added while it ignores unused fields

--- WebApp/WWW/JS/PaymentScript.js
+++ WebApp/WWW/JS/PaymentScript.js
@@ -1,5 +1,71 @@
+jsonFile = {
+    "CartID": "1",
+    "cart": {
+        "0":
+            {
+                "product":
+                    {
+                        "productName": "מלפפון",
+                        "price": "5",
+                        "description": "מה שבילו אוהב"
+
+                    },
+                "amount": "1000"
+            },
+        "1":
+            {
+                "product":
+                    {
+                        "productName": "עגבנייה",
+                        "price": "4.80",
+                        "description": "מה שדניאל אוהבת"
+
+                    },
+                "amount": "2"
+            }
+    },
+    "coupon" : {
+
+        "productName": "הנחת סטודנט",
+        "price": "10",
+        "description": "מבצע לכבוד סוף עונה"
+
+    },
+    "total sum": "1000",
+    "total amount paid": "300"
+}
+
+
+
+function loadCartFromJSON(cartJSON)
+{
+    //let CART = cartJSON.cart
+    let CART = jsonFile.cart;
+    let my_cart = document.getElementById("my-cart");
+    //firstObject.innerHTML ="";
+    let element ="";
+    for(let i in CART)
+    {
+        let product = CART[i].product;
+        let amount = CART[i].amount;
+        element += `<li class="list-group-item d-flex justify-content-between lh-condensed list-item-css ">
+                    <div>
+                        <h6 class="my-0 text-right">${product.productName}</h6>
+                        <small class="text-muted">${product.description}</small>
+                    </div>
+                    <span class="text-muted">${product.price} * ${amount} = ${amount * product.price} &#8362</span>
+                </li>`;
+    }
+
+    my_cart.innerHTML = element;
+
+}
+
+
 
 //TODO fix the function
+
+
 function onCouponsSubmission() {
 
     let coupon = document.getElementById("CouponNumber");
@@ -13,7 +79,9 @@ function onCouponsSubmission() {
         //         method: "POST",
         //         headers: { 'Content-Type': 'application/json' },
         //         body: JSON.stringify({
-        //             Coupon : coupon.value
+        //             "Coupon" : coupon.value,
+        //               "Cart_id": cart.id
+
         //
         //         })})
         //     .then(function (response) {
@@ -61,12 +129,14 @@ function OnSubmitPayment() {
 
         let approve = true;
         let elements = document.getElementsByClassName("form-control");
+        let VisaDiv = document.getElementById ("PayByVisa");
+        let isPaidByVisa = document.getElementById("credit").checked;
         // Loop over them and prevent submission in case there is an empty field
          Array.prototype.forEach.call(elements, function(element) {
              let invalidFeedbackBlock  = element.parentElement.getElementsByClassName("my-invalid-feedback")[0];
              if(invalidFeedbackBlock)
              {
-                 if(element.value == "")
+                 if(element.value == "" && (VisaDiv.contains(element) == false || isPaidByVisa))
                  {
                      approve = false;
                      invalidFeedbackBlock.style.display = "block";
@@ -77,7 +147,6 @@ function OnSubmitPayment() {
                  }
 
              }
-
 
         });
     if(approve)
@@ -94,7 +163,16 @@ function CreateJSONFromElements(elements)
         newJSON[element.id] = element.value;
 
     });
-    return newJSON;
+    //gets the radio button val
+    let paymentMethod = document.getElementsByName('paymentMethod');
+    let paymentMethod_value;
+    for(let i = 0; i < paymentMethod.length; i++){
+        if(paymentMethod[i].checked){
+            paymentMethod_value = paymentMethod[i].value;
+        }
+    }
+    newJSON[paymentMethod] = paymentMethod_value;
+
 }
 function sendPaymentDetailsToServer(elementsToSend) {
 
