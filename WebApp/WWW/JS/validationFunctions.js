3738709990de038c99323ab92c75cab0d0082b33
danielCohen123 <danielcohen150693@gmail.com>
2018-08-11 12:26:28 +0300

    validation functions

---
+++ WebApp/WWW/JS/validationFunctions.js
@@ -0,0 +1,12 @@
+function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validatePhone(phone) {
+    let isnum = /^\d+$/;
+    if(phone.length != 10 || phone.substring(0,2) != "05" ||  !isnum.test(phone)){
+        return false;
+    }
+    return true;
+}
